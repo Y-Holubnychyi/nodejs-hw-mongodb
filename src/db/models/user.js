4304c3b29710bf9dd3f05d0b687de1c12176ba70
Eugene <y.holubnychyi@gmail.com>
2025-06-26 08:43:07 -0230

    first commit: hw5-auth

---
+++ src/db/models/user.js
@@ -0,0 +1,19 @@
+import { Schema, model } from 'mongoose';
+
+const usersSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true, versionKey: false },
+);
+
+// Щоб пароль не потрапляв у JSON-відповідь:
+usersSchema.methods.toJSON = function () {
+  const obj = this.toObject();
+  delete obj.password;
+  return obj;
+};
+
+export const UsersCollection = model('users', usersSchema);
