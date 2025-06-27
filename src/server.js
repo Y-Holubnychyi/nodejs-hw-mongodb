be77eb7622f030d67f7f0d6a2fef16d7fb6ca192
Eugene <y.holubnychyi@gmail.com>
2025-06-27 08:58:20 -0230

    second commit: hw5-auth

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
 import contactsRouter from './routers/contacts.js';
+import authRouter from './routers/auth.js'; // ✅ new
 import { getEnvVar } from './utils/getEnvVar.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
@@ -25,10 +26,10 @@ export const startServer = () => {
     res.json({ message: 'Start Work' });
   });
 
+  app.use('/auth', authRouter); // ✅ нове
   app.use('/contacts', contactsRouter);
 
   app.use(notFoundHandler);
-
   app.use(errorHandler);
 
   const port = Number(getEnvVar('PORT', 3000));
