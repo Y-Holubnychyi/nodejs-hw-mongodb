40cb25a403658d8ea9a6b67ca6893681c0d49fb2
Eugene <y.holubnychyi@gmail.com>
2025-07-01 18:19:49 -0230

    fix-ctrlWrapper: hw5-auth

--- src/utils/ctrlWrapper.js
+++ src/utils/ctrlWrapper.js
@@ -1,7 +1,7 @@
 export const ctrlWrapper = (controller) => {
   return async (req, res, next) => {
     try {
-      await controller(req, res);
+      await controller(req, res, next);
     } catch (error) {
       next(error);
     }
